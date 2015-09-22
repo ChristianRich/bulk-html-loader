d0ad9b39b6face39c8880a3e0156651918140069
Christian Schlosrich <christian.schlosrich@gmail.com>
2015-09-22 16:03:42 +1000

    Support for method chaining. Additional unit tests.

--- test/index.spec.js
+++ test/index.spec.js
@@ -2,7 +2,6 @@ var assert = require('assert')
 	, should = require('should')
 	, ping = require('ping')
 	, HtmlLoader = require('../')
-	, cheerio = require('cheerio')
 	, _ = require('lodash');
 
 describe('HtmlLoader', function() {
@@ -79,7 +78,7 @@ describe('HtmlLoader', function() {
         
 			var queue = [
 				new HtmlLoader.LoaderItem('http://google.com'),
-				new HtmlLoader.LoaderItem('http://www.a-bad-url-that-will-fail dot whatever'),
+				new HtmlLoader.LoaderItem('http://www.a-bad-url-that-will-fail'),
 				new HtmlLoader.LoaderItem('https://www.google.com.au/#q=nodejs')
 			];
         
@@ -109,7 +108,7 @@ describe('HtmlLoader', function() {
 					new HtmlLoader.LoaderItem('https://www.google.com.au/#q=hello'),
 					new HtmlLoader.LoaderItem('https://www.google.com.au/#q=nodejs')
 				];
-        
+
 			htmlLoader.onItemLoadComplete(function(loaderItem, done){
 				count++;
 				done(loaderItem);
@@ -126,32 +125,72 @@ describe('HtmlLoader', function() {
 			});
         });
 
-		it('should count 3 warnings', function (done) {
-        
-			this.timeout(5000);
-			
-			var count = 0,
-				queue = [
-					new HtmlLoader.LoaderItem('https://dsfsnffnsofdsoifsofdsofso.com'),
-					new HtmlLoader.LoaderItem('https://www.google.com.au/#q=hello')
-				];
-        
-			htmlLoader.setHttpTimeout(1000);
-			htmlLoader.setNumRetries(3);
-        
-			htmlLoader.onWarning(function(loaderItem, done){
-				count++;
-				done(loaderItem);
-			});
-        
-			htmlLoader.load(queue, function(err, results){
-				assert(err === null, 'Error should not exist');
-				assert(!_.isNull(results), 'results should exist');
-				assert(_.isArray(results), 'results should be an Array');
-				assert(results.length === 2, 'expect 3 results');
-				assert(count === 3, 'onLoadComplete callback fire 3 times');
-				done();
-			});
-		});
-	});
+        it('should count 3 warnings', function (done) {
+
+            this.timeout(5000);
+
+            var count = 0,
+                queue = [
+                    new HtmlLoader.LoaderItem('https://dsfsnffnsofdsoifsofdsofso.com'),
+                    new HtmlLoader.LoaderItem('https://www.google.com.au/#q=hello')
+                ];
+
+            htmlLoader.setHttpTimeout(1000);
+            htmlLoader.setNumRetries(3);
+
+            htmlLoader.onWarning(function(loaderItem, done){
+                count++;
+                done(loaderItem);
+            });
+
+            htmlLoader.load(queue, function(err, results){
+                assert(err === null, 'Error should not exist');
+                assert(!_.isNull(results), 'results should exist');
+                assert(_.isArray(results), 'results should be an Array');
+                assert(results.length === 2, 'expect 3 results');
+                assert(count === 3, 'onLoadComplete callback fire 3 times');
+                done();
+            });
+        });
+
+        it('should support method chaining', function (done) {
+
+            this.timeout(5000);
+
+            var count = 0,
+                queue = [
+                    new HtmlLoader.LoaderItem('http://www.a-bad-url-that-will-fail'),
+                    new HtmlLoader.LoaderItem('https://www.google.com.au/#q=hello')
+                ];
+
+            var loader = new HtmlLoader()
+            .setHttpTimeout(1000)
+            .setNumRetries(3)
+            .setHttpThrottle(5)
+            .onWarning(function(loaderItem, done){
+                done(loaderItem);
+            })
+
+            .onError(function(loaderItem, done){
+                done(loaderItem);
+            })
+
+            .onItemLoadComplete(function(loaderItem, done){
+                done(loaderItem);
+            })
+
+            .load(queue, function(err, results){
+                assert(err === null, 'Error should not exist');
+                assert(!_.isNull(results), 'results should exist');
+                assert(_.isArray(results), 'results should be an Array');
+                done();
+            });
+        });
+
+        it('should create a loaderItem using htmlLoader.getLoaderItem() method', function (done) {
+            var loaderItem = htmlLoader.getLoaderItem('http://google.com', {});
+            assert(loaderItem instanceof HtmlLoader.LoaderItem);
+            done();
+        });
+    });
 });
